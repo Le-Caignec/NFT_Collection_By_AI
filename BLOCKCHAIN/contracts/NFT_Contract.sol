8f4186fb0675fa64280cb37b7d7ddf9874dc1b26
Robin <roleciq@gmail.com>
2022-05-01 13:37:06 -0400

    amélioration

--- BLOCKCHAIN/contracts/NFT_Contract.sol
+++ BLOCKCHAIN/contracts/NFT_Contract.sol
@@ -9,11 +9,11 @@ contract NFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     string public baseURI;
-    string public baseExtension = ".json";
+    string public baseExtension = ".jpeg";
     string public notRevealedUri;
     uint256 public cost = 1 ether;
-    uint256 public maxSupply = 10000;
-    uint256 public maxMintAmount = 20;
+    uint256 public maxSupply = 10;
+    uint256 public maxMintAmount = 10;
     uint256 public nftPerAddressLimit = 3;
     bool public paused = false;
     bool public revealed = false;
@@ -134,21 +134,5 @@ contract NFT is ERC721Enumerable, Ownable {
     function whitelistUsers(address[] calldata _users) public onlyOwner {
         delete whitelistedAddresses;
         whitelistedAddresses = _users;
-    }
-
-    function withdraw() public payable onlyOwner {
-        // This will pay HashLips 5% of the initial sale.
-        // You can remove this if you want, or keep it in to support HashLips and his channel.
-        // =============================================================================
-        (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-        require(hs);
-        // =============================================================================
-
-        // This will payout the owner 95% of the contract balance.
-        // Do not remove this otherwise you will not be able to withdraw the funds.
-        // =============================================================================
-        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-        require(os);
-        // =============================================================================
     }
 }
